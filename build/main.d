83bb81ba606d9ef282235404a6e9ef9205625913
Ceres <161601649+ceres-wl@users.noreply.github.com>
2026-05-26 21:00:32 -0300

    Removendo build

--- build/main.d
+++
@@ -1,3 +0,0 @@
-build/main.o: src/main.cpp src/eigenvalues.h src/matrix/matrix.h
-src/eigenvalues.h:
-src/matrix/matrix.h:
--- build/matrix/matrix.d
+++
@@ -1,2 +0,0 @@
-build/matrix/matrix.o: src/matrix/matrix.cpp src/matrix/matrix.h
-src/matrix/matrix.h:
