424f2cc648db4838a751506d3bb8f5bf390800b0
Ceres <161601649+ceres-wl@users.noreply.github.com>
2026-05-28 13:09:53 -0300

    Atualizando linearsystem.h pra usar as classes novas

--- src/linearsystem.h
+++ src/linearsystem.h
@@ -1,45 +1,44 @@
 #ifndef LINEARSYSTEM
 #define LINEARSYSTEM
 
-#include <vector>
+#include "matrix/matrix.h"
 
 namespace linear_system{
-    using namespace std;
-    
-    typedef vector<vector<double>> Matrix;
+    using namespace Mat;
+    using namespace Vec;
 
-    vector<double> solve_lu(Matrix matrix, vector<double> right_part, int n){
+    VecDouble solve_lu(Matrix matrix, VecDouble right_part, int n){
         Matrix lu;
         double sum = 0;
         for(int i = 0; i < n; i++){
             for (int j = i; j < n; j++){
                 sum = 0;
                 for (int k = 0; k < i; k++)
-                    sum += lu.at(i).at(k) * lu.at(k).at(j);
-                lu.at(i).at(j) = matrix.at(i).at(j) - sum;
+                    sum += lu.at(i, k) * lu.at(k, j);
+                lu.set(i, j, matrix.at(i, j) - sum);
             }
             for (int j = i + 1; j < n; j++){
                 sum = 0;
                 for (int k = 0; k < i; k++)
-                    sum += lu.at(j).at(k) * lu.at(k).at(i);
-                lu.at(j).at(i) = (1 / lu.at(i).at(i)) * (matrix.at(j).at(i) - sum);
+                    sum += lu.at(j, k) * lu.at(k, i);
+                lu.set(j, i, (1 / lu.at(i, i)) * (matrix.at(j, i) - sum));
             }
         }
 
-        vector<double> y;
+        VecDouble y;
         for (int i = 0; i < n; i++){
             sum = 0;
             for (int k = 0; k < i; k++)
-                sum += lu.at(i).at(k) * y.at(k);
+                sum += lu.at(i, k) * y.at(k);
             y.at(i) = right_part.at(i) - sum;
         }
-        // find solution of Ux = y
-        vector<double> x;
+        
+        VecDouble x;
         for (int i = n - 1; i >= 0; i--){
             sum = 0;
             for (int k = i + 1; k < n; k++)
-                sum += lu.at(i).at(k) * x.at(k);
-            x.at(i) = (1 / lu.at(i).at(i)) * (y.at(i) - sum);
+                sum += lu.at(i, k) * x.at(k);
+            x.at(i) = (1 / lu.at(i, i)) * (y.at(i) - sum);
         }
         return x;
     }
