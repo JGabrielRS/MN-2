ff1d09d3348cc68f5dd7da2636ddf1e5a7b3f5a3
Ceres <161601649+ceres-wl@users.noreply.github.com>
2026-07-02 20:59:14 -0300

    Adicionando método de newton-cotes aberto

--- src/integration.h
+++ src/integration.h
@@ -7,16 +7,17 @@
 
 namespace integration {
     using namespace std;
+    typedef pair<vector<double>, double> CotesWeight;
+
     double f(double x){
-        return pow((sin(2*x) + 4*pow(x, 2) + 3*x), 2);
+        return pow(x, 2);
     }
 
     double x(double xi, int i, double delta){
         return xi + i*delta;
     }
 
-    typedef pair<vector<double>, double> CotesWeight;
-    pair<vector<double>, double> get_cotes_weigth(int p){
+    CotesWeight get_closed_cotes_weigth(int p){
         switch (p)
         {
         case 1:
@@ -28,14 +29,16 @@ namespace integration {
         case 3:
             return {{1, 3, 3, 1}, 3.0f/8};
             break;
+        case 4:
+            return {{7, 32, 12, 32, 7}, 2.0f/45};
+            break;
         default:
             return {};
             break;
         }
     }
 
-    double integrate(double x0, double xf, int p){
-        CotesWeight points = get_cotes_weigth(p);
+    double integrate(double x0, double xf, int p, CotesWeight& points){
         double delta = (xf - x0)/p;
 
         double sumn = 0;
@@ -47,13 +50,51 @@ namespace integration {
         return (delta*points.second)*sumn;
     }
     
-    double newton_cotes(int a, int b, double n, int p){
+    double closed_newton_cotes(int a, int b, double n, int p){
         double delta = (b - a)/n;
         double as = 0;
+
+        CotesWeight points = get_closed_cotes_weigth(p);
+
         for (int i = 0; i < n; i++){
             double xi = a + i * delta;
             double xf = xi + delta;
-            as += integrate(xi, xf, p);
+            as += integrate(xi, xf, p, points);
+        }
+        return as;
+    }
+
+    CotesWeight get_open_cotes_weigth(int p){
+        switch (p)
+        {
+        case 1:
+            return {{0, 1}, 2.0f};
+            break;
+        case 2:
+            return {{0, 1, 1}, 3.0f/2};
+            break;
+        case 3:
+            return {{0, 2, -1, 2}, 4.0f/3};
+            break;
+        case 4:
+            return {{0, 11, 1, 1, 11}, 5.0f/24};
+            break;
+        default:
+            return {};
+            break;
+        }
+    } 
+
+    double open_newton_cotes(int a, int b, double n, int p){
+        double delta = (b - a)/n;
+        double as = 0;
+
+        CotesWeight points = get_open_cotes_weigth(p);
+
+        for (int i = 0; i < n; i++){
+            double xi = a + i * delta;
+            double xf = xi + delta;
+            as += integrate(xi, xf, p+1, points);
         }
         return as;
     }
