73cfac99fe83d509aada7989f349782eb381e6c0
Ceres <161601649+ceres-wl@users.noreply.github.com>
2026-07-02 22:48:04 -0300

    Adicionando gauss-laguerre e gauss-chebyshev
    consertando os pesos dos outros tbm, que tava errado

--- src/integration.h
+++ src/integration.h
@@ -8,12 +8,24 @@
 namespace integration {
     using namespace std;
     typedef pair<vector<double>, double> CotesWeight;
+    typedef pair<vector<double>, vector<double>> GaussWeights;
 
     double f(double x){
         return exp(x);
-        
+
         // pro gauss hermite
-        // return exp(-pow(x, 2));
+        // return cos(x);
+    }
+
+    // === Auxiliares ===
+
+    double sum_gauss(GaussWeights points, int n){
+        double sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += f(points.first.at(i))*points.second.at(i);
+        }
+
+        return sum;
     }
 
     // === Newton-Cotes Fechado ===
@@ -93,14 +105,11 @@ namespace integration {
     }
 
     // === Gauss-Legendre ===
-    typedef pair<vector<double>, vector<double>> GaussWeights;
 
     GaussWeights get_legendre_weights(int n){
         double p1, p2, w1, w2;
         switch (n)
         {
-        case 1:
-            return {{0}, {2}};
         case 2:
             p1 = 1.0/sqrt(3);
             return {{-p1, p1}, {1, 1}};
@@ -131,24 +140,22 @@ namespace integration {
     }
 
     // === Gauss Hermite === 
-
     GaussWeights get_hermite_weights(int n){
+        // https://www.efunda.com/math/num_integration/findgausshermite.cfm
         double p1, p2, w1, w2;
         switch (n)
         {
-        case 1:
-            return {{0}, {1.77245}};
         case 2:
             p1 = 0.707107;
-            return {{-p1, p1}, {1.46114, 1.46114}};
+            return {{-p1, p1}, {0.886227, 0.886227}};
         case 3:
             p1 = 1.22474;
-            return {{-p1, 0, p1}, {1.32393, 1.18164, 1.32393}};
+            return {{-p1, 0, p1}, {0.295409, 1.18164, 0.295409}};
         case 4:
             p1 = 1.65068;
             p2 = 0.524648;
-            w1 = 1.24023;
-            w2 = 1.05996;
+            w1 = 0.0813128;
+            w2 = 0.804914;
             return {{-p1, -p2, p2, p1}, {w1, w2, w2, w1}};
         default:
             return {};
@@ -157,13 +164,51 @@ namespace integration {
 
     double gauss_hermite(int n){
         GaussWeights points = get_hermite_weights(n);
+        return sum_gauss(points, n);
+    }
 
-        double sum = 0;
-        for(int i = 0; i < n; i++){
-            sum += f(points.first.at(i))*points.second.at(i);
+    // === Gauss-Laguerre ===
+
+    GaussWeights get_laguerre_weights(int n){
+        // https://mathworld.wolfram.com/Laguerre-GaussQuadrature.html
+        switch (n)
+        {
+        case 2:
+            return {{0.585786, 3.41421}, {0.853553, 0.146447}};
+        case 3:
+            return {{0.415775, 2.29428, 6.28995}, {0.711093, 0.278518, 0.0103893}};
+        case 4:
+            return {{0.322548, 1.74576, 4.53662, 9.39507}, {0.603154, 0.357419, 0.0388879, 0.000539295}};
+        default:
+            return {};
         }
+    }
 
-        return sum;
+    double gauss_laguerre(int n){
+        GaussWeights points = get_laguerre_weights(n);
+        return sum_gauss(points, n);
+    }
+
+    // === Gauss-Chebyshev ===
+
+    GaussWeights get_chebyshev_weights(int n){
+        // https://mathworld.wolfram.com/Chebyshev-GaussQuadrature.html
+        switch (n)
+        {
+        case 2:
+            return {{-0.707107, 0.707107}, {1.5708, 1.5708}};
+        case 3:
+            return {{-0.866025, 0, 0.866025}, {1.0472, 1.0472, 1.0472}};
+        case 4:
+            return {{-0.92388, -0.382683, 0.382683, 0.92388}, {0.785398, 0.785398, 0.785398, 0.785398}};
+        default:
+            return {};
+        }
+    }
+
+    double gauss_chebyshev(int n){
+        GaussWeights points = get_chebyshev_weights(n);
+        return sum_gauss(points, n);
     }
 }
 
